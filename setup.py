34c6048313943bfc6e60fb51e97a4bc7c1d159bd
Steve Huff <steve.huff@runkeeper.com>
2016-09-29 11:49:20 -0400

    set correct version number, update CONTRIBUTORS

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='libhoney',
-      version='0.1.0',
+      version='1.0.0',
       description='Python library for sending data to Honeycomb',
       url='https://github.com/honeycombio/libhoney-py',
       author='Honeycomb.io',
