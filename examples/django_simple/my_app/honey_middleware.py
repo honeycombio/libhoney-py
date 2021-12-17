606bd0f1460e55a0a585ca2b6373ec7958fe8199
Vera Reynolds <verareynolds@honeycomb.io>
2021-12-17 11:08:49 -0700

    upgrade simple example

--- examples/django_simple/my_app/honey_middleware.py
+++ examples/django_simple/my_app/honey_middleware.py
@@ -1,27 +1,30 @@
 import os
+
 import libhoney
 
 
 class HoneyMiddleware(object):
-    def __init__(self):
+    def __init__(self, get_response):
+        self.get_response = get_response
         libhoney.init(writekey=os.environ["HONEYCOMB_API_KEY"],
-                      dataset=os.environ["HONEYCOMB_DATASET"])
+                      dataset=os.environ.get("HONEYCOMB_DATASET", "django-example"),
+                      api_host=os.environ.get("HONEYCOMB_API_ENDPOINT", "https://api.honeycomb.io"))
 
-
-    def process_request(self, request):
+    def __call__(self, request):
+        is_ajax = request.headers.get('x-requested-with') == 'XMLHttpRequest'
         ev = libhoney.new_event(data={
             "method": request.method,
             "scheme": request.scheme,
             "path": request.path,
             "query": request.GET,
             "isSecure": request.is_secure(),
-            "isAjax": request.is_ajax(),
-            "isUserAuthenticated": request.user.is_authenticated(),
+            "isAjax": is_ajax,
+            "isUserAuthenticated": request.user.is_authenticated,
             "username": request.user.username,
             "host": request.get_host(),
             "ip": request.META['REMOTE_ADDR'],
         })
         ev.send()
 
-        return None
-
+        response = self.get_response(request)
+        return response
