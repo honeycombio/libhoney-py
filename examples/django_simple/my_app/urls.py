66101a696d96e5318b7217cd4cb93a6fe9eed636
Vera Reynolds <vreynolds@users.noreply.github.com>
2021-12-20 10:48:55 -0700

    Upgrade django examples: simple (#96)
    * Bump django from 1.11.29 to 2.2.24 in /examples/django_simple
    
    Bumps [django](https://github.com/django/django) from 1.11.29 to 2.2.24.
    - [Release notes](https://github.com/django/django/releases)
    - [Commits](https://github.com/django/django/compare/1.11.29...2.2.24)
    
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>

--- examples/django_simple/my_app/urls.py
+++ examples/django_simple/my_app/urls.py
@@ -1,7 +1,7 @@
-from django.conf.urls import include, url
+from django.urls import path
 
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.index),
+    path('', views.index, name='index'),
 ]
